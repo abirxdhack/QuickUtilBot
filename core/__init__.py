5ca873072ee7f46ad3fb7375463ccb1976575f23
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-08-17 12:56:00 +0600

    Update Copyright Headers!!

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,6 @@
+#Copyright @ISmartCoder
+#Updates Channel t.me/TheSmartDev
 from .mongo import user_activity_collection
 from .database import auth_admins, banned_users, restart_messages
 from .start import setup_start_handler
+

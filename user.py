af8d52621871bbd22f6426cba43a7b92bda8c95b
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-08-19 22:35:24 +0600

    Fixed  Telethon __init__ Issue

--- user.py
+++ user.py
@@ -1,11 +1,14 @@
-#Copyright @ISmartCoder
-#Updates Channel t.me/TheSmartDev
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from utils import LOGGER
-from config import SESSION_STRING
+from config import SESSION_STRING, API_ID, API_HASH
+
 LOGGER.info("Creating User Client From SESSION_STRING")
+
 user = TelegramClient(
-    StringSession(SESSION_STRING)
+    StringSession(SESSION_STRING),
+    API_ID,
+    API_HASH
 )
+
 LOGGER.info("User Client Successfully Created!")
